e25a4c557cb9700ee8cde53946dbad84f07dfc54
Wellington Marques <101907715+Wellijr@users.noreply.github.com>
2023-03-11 01:25:45 +0000

    Create Aula3 Contato.java

---
+++ Aula3 Contato.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista;
+
+/**
+ *
+ * @author wellington.omjunior
+ */
+public class Contato {
+    private String nome,tel;
+    private int id;
+    
+    public Contato(String nome,String tel){
+        this.nome = nome; 
+        this.tel = tel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" + "nome=" + nome + ", tel=" + tel + ", id=" + id + '}';
+    }
